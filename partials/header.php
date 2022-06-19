b8a7c3b466b9115e54aa1a2c991cec9d8950d1f0
katherin <ktlamas@gmail.com>
2022-06-18 21:54:30 -0500

    se creo estructura para header

---
+++ partials/header.php
@@ -0,0 +1,27 @@
+
+<header class="header-main">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div class="logo">
+                    <img src="../asset/logos/kj.png" alt="logo" width="100px">
+                </div>
+            </div>
+
+            
+
+            <div class="col-12 col-md-6 d-block d-md-none">
+                <nav>
+                    <ul>
+                        <li><a href="#">inicio</a></li>
+                        <li><a href="#">servicios</a></li>
+                        <li><a href="#">nosotros</a></li>
+                        <li><a href="#">productos</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+           
+        </div>
+    </div>
+</header>
